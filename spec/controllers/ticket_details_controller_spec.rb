5ad7c0ae9c4130877828928d544a24cce9eccbda
muraleekg <muraleekrishnagc@gmail.com>
2014-08-21 09:48:35 +0530

    with controller test

--- spec/controllers/ticket_details_controller_spec.rb
+++ spec/controllers/ticket_details_controller_spec.rb
@@ -2,8 +2,15 @@ require "spec_helper"
 
 describe TicketDetailsController do
   let(:ticket_detail) { FactoryGirl.create(:ticket_detail) }
-
-  it "should create a ticket_detail" do
-    expect { xhr :post, :create, { "ticket_detail" => {"from" => ticket_detail.from, "to" => ticket_detail.to, "journey_date" => ticket_detail.journey_date, "direction" => ticket_detail.direction }} }.to change(TicketDetail, :count)
+  describe "with valid information" do
+    it "should create a ticket_detail" do
+      expect { xhr :post, :create, { "ticket_detail" => {"from" => ticket_detail.from, "to" => ticket_detail.to, "journey_date" => ticket_detail.journey_date, "direction" => ticket_detail.direction }} }.to change(TicketDetail, :count)
     end
+  end
+  describe "without valid data" do
+     it "should not create a ticket_detail" do
+      expect { xhr :post, :create, { "ticket_detail" => {"from" => " ", "to" => " ", "journey_date" => " ", "direction" => " " }} }.not_to change(TicketDetail, :count)
+     end
+  end
+     
 end
